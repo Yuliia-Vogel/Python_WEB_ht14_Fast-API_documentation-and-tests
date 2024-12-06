b78152bb962bc8a953734c1e91dbb2acbec877e1
Sergstm <sstm@ukr.net>
2024-12-06 17:04:19 +0200

    pytest-started-confect.py-file-added

---
+++ tests/routes/test_pytest_route_auth.py
@@ -0,0 +1,16 @@
+from unittest.mock import MagicMock
+
+from src.database.models import User
+
+
+def test_create_user(client, user, monkeypatch):
+    mock_send_email = MagicMock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    response = client.post(
+        "/api/auth/signup",
+        json=user,
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["user"]["email"] == user.get("email")
+    assert "id" in data["user"]
